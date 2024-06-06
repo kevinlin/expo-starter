95355f054343a30d9d7984c3e92d0b9e408d6d90
Kevin Lin <kevin.lin@zuhlke.com>
2024-06-06 19:21:16 +0800

    Switch from `react-native-msal2` to `expo-auth-session`

--- app.config.ts
+++ app.config.ts
@@ -33,7 +33,7 @@ export default {
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
-    scheme: "com.bostonscientific.jarvis-sakura-mobile.stage",
+    scheme: getUniqueIdentifier(),
     splash: {
       image: "./assets/splash.png",
       resizeMode: "contain",
--- components/pages/LoginPage.tsx
+++ components/pages/LoginPage.tsx
@@ -1,119 +1,82 @@
-import React, { useEffect, useState } from "react";
 import {
-  ActivityIndicator,
-  Alert,
-  Platform,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import PublicClientApplication, {
-  MSALConfiguration,
-  MSALInteractiveParams,
-} from "react-native-msal2";
+  exchangeCodeAsync,
+  useAuthRequest,
+  useAutoDiscovery,
+} from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
+import * as React from "react";
+import { Button, Platform, SafeAreaView, Text } from "react-native";
 
-const config: MSALConfiguration = {
-  auth: {
-    clientId: "27225af2-948c-4229-afb6-82bc1e9ac211",
-    authority:
-      "https://login.microsoftonline.com/b5b8b483-5597-4ae7-8e27-fcc464a3b584",
-    redirectUri: Platform.select({
-      android:
-        "msauth://com.bostonscientific.jarvis-sakura-mobile.stage/cEuBfTV24qnKzfDEzC%2F4t8Z6SLg%3D", // ex: "msauth://com.package/Xo8WBi6jzSxKDVR4drqm84yr9iU%3D"
-      ios: "msauth.com.bostonscientific.jarvis-sakura-mobile.stage://auth", // ex: "msauth.com.package.app://auth"
-      default: "http://localhost:3000",
-    }),
-  },
-  
-};
-const scopes = ['user.Read'];
+// const config: MSALConfiguration = {
+//   auth: {
+//     clientId: "27225af2-948c-4229-afb6-82bc1e9ac211",
+//     authority:
+//       "https://login.microsoftonline.com/b5b8b483-5597-4ae7-8e27-fcc464a3b584",
+//     redirectUri: Platform.select({
+//       android:
+//         "msauth://com.bostonscientific.jarvis-sakura-mobile.stage/cEuBfTV24qnKzfDEzC%2F4t8Z6SLg%3D", // ex: "msauth://com.package/Xo8WBi6jzSxKDVR4drqm84yr9iU%3D"
+//       ios: "msauth.com.bostonscientific.jarvis-sakura-mobile.stage://auth", // ex: "msauth.com.package.app://auth"
+//       default: "http://localhost:3000",
+//     }),
+//   },
+// };
 
-const LoginPage = () => {
-  const [msalClient, setMsalClient] = useState<PublicClientApplication | null>(
-    null
+WebBrowser.maybeCompleteAuthSession();
+
+export default function App() {
+  // Endpoint
+  const discovery = useAutoDiscovery(
+    "https://login.microsoftonline.com/b5b8b483-5597-4ae7-8e27-fcc464a3b584/v2.0"
   );
-  const [loading, setLoading] = useState(true);
+  const clientId = "27225af2-948c-4229-afb6-82bc1e9ac211";
+  const redirectUri = Platform.select({
+    android:
+      "msauth://com.bostonscientific.jarvis-sakura-mobile.stage/cEuBfTV24qnKzfDEzC%2F4t8Z6SLg%3D", // ex: "msauth://com.package/Xo8WBi6jzSxKDVR4drqm84yr9iU%3D"
+    ios: "msauth.com.bostonscientific.jarvis-sakura-mobile.stage://auth", // ex: "msauth.com.package.app://auth"
+    default: "http://localhost:3000",
+  });
 
-  useEffect(() => {
-    const initializeMsalClient = async () => {
-      const client = new PublicClientApplication(config);
-      try {
-        await client.init();
-        setMsalClient(client);
-      } catch (error) {
-        console.error(
-          "Error initializing the msalClient, check your config.",
-          error
-        );
-        Alert.alert(
-          "Initialization Error",
-          "There was an error initializing the authentication client. Please try again later."
-        );
-      } finally {
-        setLoading(false);
-      }
-    };
+  // We store the JWT in here
+  const [token, setToken] = React.useState<string | null>(null);
 
-    initializeMsalClient();
-  }, []);
-
-  const doLogin = async () => {
-    if (!msalClient) {
-      Alert.alert("Error", "MSAL Client not initialized.");
-      return;
-    }
-
-    try {
-      const params: MSALInteractiveParams = { scopes };
-      const result = await msalClient.acquireToken(params);
-      if (result) {
-        Alert.alert("Login Successful", `Welcome, ${result.account.username}`);
-      } else {
-        Alert.alert("Login Failed", "No result returned.");
-      }
-    } catch (error) {
-      console.error("Error during login", error);
-      Alert.alert(
-        "Login Error",
-        "An error occurred during login. Please try again."
-      );
-    }
-  };
-
-  if (loading) {
-    return (
-      <View style={styles.container}>
-        <ActivityIndicator size="large" color="#007bff" />
-      </View>
-    );
-  }
+  // Request
+  const [request, , promptAsync] = useAuthRequest(
+    {
+      clientId,
+      scopes: ["openid", "profile", "email", "offline_access"],
+      redirectUri,
+    },
+    discovery
+  );
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={doLogin}>
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView>
+      <Button
+        disabled={!request}
+        title="Login"
+        onPress={() => {
+          promptAsync().then((codeResponse) => {
+            console.log("AuthSessionResult", codeResponse);
+            if (request && codeResponse?.type === "success" && discovery) {
+              exchangeCodeAsync(
+                {
+                  clientId,
+                  code: codeResponse.params.code,
+                  extraParams: request.codeVerifier
+                    ? { code_verifier: request.codeVerifier }
+                    : undefined,
+                  redirectUri,
+                },
+                discovery
+              ).then((res) => {
+                console.log("TokenResponse", res);
+                setToken(res.accessToken);
+              });
+            }
+          });
+        }}
+      />
+      <Text>{token}</Text>
+    </SafeAreaView>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f5fcff",
-  },
-  button: {
-    backgroundColor: "#007bff",
-    padding: 15,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 16,
-  },
-});
-
-export default LoginPage;
+}
